e810e08812abb5b31d7b0f2ef3f128580a19c325
YiQian Zhang <xufezyq@gmail.com>
2026-02-03 17:05:50 +0800

    修改如何发消息的选择

--- core/wechat_instance.py
+++ core/wechat_instance.py
@@ -3,13 +3,14 @@
 """
 from wxauto import WeChat
 
-_use_pywechat = True
+_use_wxauto = False
 _wx = None # 全局WeChat单例对象，所有模块都可以导入使用
 
 def get_wechat():
     """获取WeChat实例，延迟初始化"""
-    if _use_pywechat:
-        return None  # 使用 pywechat 时不需要 WeChat 实例
+    if not _use_wxauto:
+        return None  # 使用 wxauto 时不需要 WeChat 实例
+    
     global _wx
     if _wx is None:
         _wx = WeChat()
@@ -17,9 +18,14 @@ def get_wechat():
 
 def init_wechat():
     """显式初始化WeChat实例"""
-    if _use_pywechat:
-        return None  # 使用 pywechat 时不需要 WeChat 实例
+    if not _use_wxauto:
+        return None  # 使用 wxauto 时不需要 WeChat 实例
+    
     global _wx
     if _wx is None:
         _wx = WeChat()
     return _wx
+
+def is_using_wxauto():
+    """检查是否使用 wxauto"""
+    return _use_wxauto
